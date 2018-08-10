4150c3a8008fcfef608336829b094e6f2520b899
KatMerr <mattjknerr@gmail.com>
2018-08-09 23:36:18 -0400

    Added Fonts, Lorem Ipsum, and basic content styling

--- client/src/components/pages/attire.js
+++ client/src/components/pages/attire.js
@@ -1,3 +1,7 @@
 import React, { Component } from 'react'
 
-export default () => <div>attire</div>
+export default () => <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id lacinia velit, ut tincidunt ipsum. Vivamus ante est, accumsan vel dictum vitae, egestas ac lectus. Vestibulum ultricies placerat finibus. Ut vel iaculis felis. Nam vitae sollicitudin erat. In feugiat, purus et laoreet sagittis, mauris urna condimentum mi, id mattis sapien ligula tincidunt lectus. Phasellus pharetra erat a arcu hendrerit, id laoreet diam pharetra. Ut congue tellus et ultrices fringilla.
+
+Maecenas iaculis felis velit, at volutpat sapien congue nec. Fusce in tincidunt ipsum. Nunc nec facilisis turpis. Maecenas blandit felis et lobortis lacinia. In lobortis arcu quis risus sagittis, non tempor ante semper. Sed malesuada elit in diam tristique, at gravida nunc gravida. Cras risus neque, rutrum id egestas ac, blandit sed lectus. Sed condimentum sed odio at auctor. Pellentesque ornare facilisis ipsum, non sollicitudin enim gravida id. Quisque in nibh lectus. Quisque tempus ornare sapien suscipit cursus. In hac habitasse platea dictumst.
+
+Sed mi ligula, convallis in placerat sit amet, facilisis sit amet odio. Vestibulum turpis tellus, convallis dignissim erat non, euismod mollis leo. Suspendisse quam lacus, gravida eget diam ultrices, eleifend ultrices est. Morbi vitae lorem dolor. Maecenas nec massa consequat, pharetra est quis, aliquam neque. Fusce aliquet velit at libero facilisis condimentum. Nunc semper viverra dapibus. Vivamus id tristique nibh. Duis tincidunt mi vel hendrerit interdum. Nullam ac enim dui. Nunc ultrices augue quis mi imperdiet viverra. Nunc hendrerit eros augue, sed dictum dui dignissim id.</div>
