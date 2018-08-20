38ce3f07082ed776be5ff910f5e26ef28508b67c
Matty Ice <mattjknerr@gmail.com>
2018-08-19 23:48:03 -0400

    Added basic app loader

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,16 +3,36 @@ import Navigation from './navigation'
 import Content from './content'
 import Background from './background'
 import Footer from './footer'
+import Loader from './loader'
 import '../css/boostrap/css/bootstrap.css'
 import '../less/styles.less'
 
-const App = () => (
-    <div>
-        <Background />
-        <Navigation />
-        <Content />
-        <Footer />
-    </div>
-)
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => this.setState({ loading: false }), 1000);
+    }
+
+    render() {
+        const { loading } = this.state;
+
+        return (
+            <div>
+                <Loader loading={loading} />
+                <Background />
+                <Navigation />
+                <Content />
+                <Footer />
+            </div>
+        )
+    }
+}
 
 export default App
--- client/src/components/content.js
+++ client/src/components/content.js
@@ -21,7 +21,6 @@ class Content extends Component {
         return (
             <div className="content-container" 
                  style={{minHeight: `${this.state.minHeight}px`, height: `${this.state.minHeight}px`, backgroundImage: `url(${PlanksBackground})`}}>
-                <div style={{backgroundColor: 'white', height: '100%', width: '100%', opacity: '.5', position: 'absolute', top: 0, left: 0, zIndex: 2}}></div>
                 <div className="content-sub-container" style={{position: 'relative', zIndex: 3, backgroundColor: 'rgba(255, 255, 255, .7)', height: '100%', maxWidth: "1000px", margin: '0 auto'}}>
                     <Switch>
                         <Route exact path="/" component={Home} />
