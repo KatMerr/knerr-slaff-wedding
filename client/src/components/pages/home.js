9ced12c11e5fc114e87e1f586d833684dd17a41b
KatMerr <mattjknerr@gmail.com>
2018-12-27 22:48:36 -0500

    Added Decorative pieces

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -4,7 +4,7 @@ import Placeholder from '../../../../images/cloud-logo.png'
 export default () => (
     <div>
         <h2>Home</h2> 
-        <div className="content-description">sit amet, consectetur adipiscing elit. Aliquam id lacinia velit, ut tincidunt ipsum. Vivamus ante est, accumsan vel dictum vitae, egestas ac lectus. Vestibulum ultricies placerat finibus. Ut vel iaculis felis. Nam vitae sollicitudin erat. In feugiat, purus et laoreet sagittis, mauris urna condimentum mi, id mattis sapien ligula tincidunt lectus. Phasellus pharetra erat a arcu hendrerit, id laoreet diam pharetra. Ut congue tellus et ultrices fringilla.
+        sit amet, consectetur adipiscing elit. Aliquam id lacinia velit, ut tincidunt ipsum. Vivamus ante est, accumsan vel dictum vitae, egestas ac lectus. Vestibulum ultricies placerat finibus. Ut vel iaculis felis. Nam vitae sollicitudin erat. In feugiat, purus et laoreet sagittis, mauris urna condimentum mi, id mattis sapien ligula tincidunt lectus. Phasellus pharetra erat a arcu hendrerit, id laoreet diam pharetra. Ut congue tellus et ultrices fringilla.
         <div className="profile">
             <figure className='headshot'>
                 <img src={Placeholder} />
@@ -14,7 +14,6 @@ export default () => (
             <div className='name'>Jimmy Johnson</div>
             s velit, at volutpat sapien congue nec. Fusce in tincidunt ipsum. Nunc nec facilisis turpis. Maecenas blandit felis et lobortis lacinia. In lobortis arcu quis risus sagittis, non tempor ante semper. Sed malesuada elit in diam tristique, at gravida nunc gravida. Cras risus neque, rutrum id egestas ac, blandit sed lectus. Sed condimentum sed odio at auctor. Pellentesque ornare facilisis ipsum, non sollicitudin enim gravida id. Quisque in nibh lectus. Quisque tempus ornare sapien suscipit cursus. In hac habitasse platea dictumst.
         Sed mi ligula, convall
-            </div>
         </div>
         lis felis velit, at volutpat sapien congue nec. Fusce in tincidunt ipsum. Nunc nec facilisis turpis. Maecenas blandit felis et lobortis lacinia. In lobortis arcu quis risus sagittis, non tempor ante semper. Sed malesuada elit in diam tristique, at gravida nunc gravida. Cras risus neque, rutrum id egestas ac, blandit sed lectus. Sed condimentum sed odio at auctor. Pellentesque ornare facilisis ipsum, non sollicitudin enim gravida id. Quisque in nibh lectus. Quisque tempus ornare sapien suscipit cursus. In hac habitasse platea dictumst.
         Sed mi ligula, colis felis velit, at volutpat sapien congue nec. Fusce in tincidunt ipsum. Nunc nec facilisis turpis. Maecenas blandit felis et lobortis lacinia. In lobortis arcu quis risus sagittis, non tempor ante semper. Sed malesuada elit in diam tristique, at gravida nunc gravida. Cras risus neque, rutrum id egestas ac, blandit sed lectus. Sed condimentum sed odio at auctor. Pellentesque ornare facilisis ipsum, non sollicitudin enim gravida id. Quisque in nibh lectus. Quisque tempus ornare sapien suscipit cursus. In hac habitasse platea dictumst.
