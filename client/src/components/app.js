629b0a92346ed96c733d56eecfc76517e536b021
KatMerr <mattjknerr@gmail.com>
2019-01-06 20:51:23 -0500

    Style Changes

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,7 +3,6 @@ import Navigation from './navigation'
 import Content from './content'
 import Background from './background'
 import Footer from './footer'
-import '../css/boostrap/css/bootstrap.css'
 import '../less/styles.less'
 
 class App extends Component {
