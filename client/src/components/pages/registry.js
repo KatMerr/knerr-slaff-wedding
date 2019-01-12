8c19abe6d1430141292b4716ab968b5bd45af2fd
KatMerr <mattjknerr@gmail.com>
2019-01-12 16:00:01 -0500

    Finished Registry Page

--- client/src/components/pages/registry.js
+++ client/src/components/pages/registry.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import KnotLogo from '../../../../images/the-knot-logo.gif'
+import TargetLogo from '../../../../images/target-logo.gif'
+import MacysLogo from '../../../../images/macys-logo.gif'
+import WestElmLogo from '../../../../images/west-elm-logo.gif'
 
 export default () => (
     <div>
         <h2>Registry</h2>
         <div>Matt’s unprovoked dislike of TheKnot websites means this website isn’t one, but they do make for good registry collectors. If you would like to view a general summary of everything on our registry or contribute to our Cash Registry, click the button below:</div>
         <div className='button-container'>
-            <div className='registry-button placeholder'>The Knot</div>
+            <div className='registry-button placeholder'><img src={KnotLogo} /></div>
         </div>
         <div>Otherwise join the protest and click directly to the individual store registeries:</div>
         <div className='button-container'>
-            <div className='registry-button placeholder'>Target</div>
-            <div className='registry-button placeholder'>West Elm</div>
-            <div className='registry-button placeholder'>Macy's</div>
+            <div className='registry-button placeholder'><img src={TargetLogo} /></div>
+            <div className='registry-button placeholder'><img src={WestElmLogo} /></div>
+            <div className='registry-button placeholder'><img src={MacysLogo} /></div>
         </div>
     </div>
 )
